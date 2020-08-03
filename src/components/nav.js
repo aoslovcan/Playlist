347472e49b5167cf94f7978d9c316ce56e18c991
Antonija Oslovčan <oslovcana22@gmail.com>
2020-08-03 18:47:54 +0200

    App setupp

---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styleComponent.css';
+
+function Nav() {
+    return (
+        <>
+            <div className="navigation">
+                <h3><FontAwesomeIcon icon='music' color='#ffc0cb' /></h3>
+                <h3> <FontAwesomeIcon icon='music' color='#ffff00' /> Dobrodošli u TopPop <FontAwesomeIcon icon='music' color='#008000' />
+                </h3>
+                <h3><FontAwesomeIcon icon='music' color='#0000ff' /> </h3>
+            </div>
+
+
+        </>
+    );
+}
+
+export default Nav;
---
+++ src/routers/Home.js
@@ -0,0 +1,16 @@
+import React,  { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Nav from '../components/nav';
+
+
+class Home extends Component {
+  render(){
+      return(
+          <>
+          <Nav/>
+          </>
+      );
+  }
+}
+
+export default Home;
